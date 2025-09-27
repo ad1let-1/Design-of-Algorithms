e2bc353a2d8efc0a8d57cc5a94fe1537d2ef5b48
Nuradilet Mustafa <mustafaadilett@gmail.com>
2025-09-28 03:07:48 +0500

    Resolve QuickSort.java conflicts

--- src/main/java/org/adilet/dnc/sort/QuickSort.java
+++ src/main/java/org/adilet/dnc/sort/QuickSort.java
@@ -16,7 +16,7 @@ public class QuickSort {
     }
 
     private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
+        int pivot = arr[high]; 
         int i = low - 1;
         for (int j = low; j < high; j++) {
             if (arr[j] <= pivot) {
