64d05ff59fdc302b4ba196530e4ae99c75744152
Nuradilet Mustafa <mustafaadilett@gmail.com>
2025-09-28 02:15:08 +0500

    Resolve conflicts: remove MergeSort references from Main.java, keep QuickSort

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 package org.adilet.dnc;
 
-import org.adilet.dnc.sort.MergeSort;
 import org.adilet.dnc.sort.QuickSort;
 
 import java.util.Arrays;
@@ -10,18 +9,13 @@ public class Main {
     public static void main(String[] args) {
         // создаём случайный массив
         Random rnd = new Random(42);
-        int[] arr1 = rnd.ints(20, 0, 100).toArray();
-        int[] arr2 = arr1.clone();
+        int[] arr = rnd.ints(20, 0, 100).toArray();
 
         System.out.println("Исходный массив:");
-        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr));
 
-        QuickSort.sort(arr1);
+        QuickSort.sort(arr);
         System.out.println("QuickSort:");
-        System.out.println(Arrays.toString(arr1));
-
-        MergeSort.sort(arr2);
-        System.out.println("MergeSort:");
-        System.out.println(Arrays.toString(arr2));
+        System.out.println(Arrays.toString(arr));
     }
 }
