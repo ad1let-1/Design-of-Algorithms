f6bfa6f2f37631eb6b6edd22decec7a28c1ad6c3
Nuradilet Mustafa <mustafaadilett@gmail.com>
2025-09-28 00:22:20 +0500

    Resolve conflicts for quick-sort branch

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,27 @@
+package org.adilet.dnc;
 
+import org.adilet.dnc.sort.MergeSort;
+import org.adilet.dnc.sort.QuickSort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        // создаём случайный массив
+        Random rnd = new Random(42);
+        int[] arr1 = rnd.ints(20, 0, 100).toArray();
+        int[] arr2 = arr1.clone();
+
+        System.out.println("Исходный массив:");
+        System.out.println(Arrays.toString(arr1));
+
+        QuickSort.sort(arr1);
+        System.out.println("QuickSort:");
+        System.out.println(Arrays.toString(arr1));
+
+        MergeSort.sort(arr2);
+        System.out.println("MergeSort:");
+        System.out.println(Arrays.toString(arr2));
+    }
+}
